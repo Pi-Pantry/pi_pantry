20d499d721a058d3a6401d4e9c87b1070d66ca17
George Ceja <net.george.sky@gmail.com>
2018-04-16 12:46:55 -0700

    Added view configuration to manage items

---
+++ pi_pantry/sample_data/__init__.py
@@ -0,0 +1,134 @@
+MOCK_DATA = {
+    [
+        {
+          "code": "OK",
+          "offset": 0,
+          "results_count": 1,
+          "results": [
+            {
+              "size": "1.76 oz",
+              "gtins": [
+                "00638102201010"
+              ],
+              "sem3_id": "4jcTUI5OISa0q28uG4qmAA",
+              "cat_id": "22529",
+              "width": "292.10",
+              "length": "520.70",
+              "height": "165.10",
+              "price_currency": "USD",
+              "brand": "Zone",
+              "upc": "638102201010",
+              "price": "15.51",
+              "model": "63162",
+              "created_at": 1375016061,
+              "name": "Zone Nutrition Bar, Choc Pnt Bt, 1.76-Ounce (Pack of 12)",
+              "mpn": "57975",
+              "messages": [],
+              "geo": [
+                "usa"
+              ],
+              "manufacturer": "Millbrook Distribution Services Inc.",
+              "ean": "0638102201010",
+              "updated_at": 1521962793,
+              "images": [
+                "http://sem3-idn.s3-website-us-east-1.amazonaws.com/19604022db74111db69d4aab8209b143,0.jpg"
+              ],
+              "weight": "589670.08",
+              "sitedetails": [
+                {
+                  "recentoffers_count": 3,
+                  "latestoffers": [
+                    {
+                      "price": "15.51",
+                      "lastrecorded_at": 1521962700,
+                      "shipping": "759",
+                      "isactive": 1,
+                      "currency": "USD",
+                      "firstrecorded_at": 1521962700,
+                      "id": "4MoRjSrlOqkEusOEM8YUU6",
+                      "condition": "new",
+                      "availability": "Available",
+                      "seller": "Bargain The People"
+                    },
+                    {
+                      "price": "24.95",
+                      "lastrecorded_at": 1521962700,
+                      "isactive": 1,
+                      "currency": "USD",
+                      "firstrecorded_at": 1521962700,
+                      "id": "5595a0Oe5A68meqOqA2sqe",
+                      "condition": "new",
+                      "availability": "Available",
+                      "seller": "KRHSA"
+                    },
+                    {
+                      "price": "24.52",
+                      "lastrecorded_at": 1521962700,
+                      "isactive": 1,
+                      "currency": "USD",
+                      "firstrecorded_at": 1521962700,
+                      "id": "0cuYBeEHERaAGiU40MqeSc",
+                      "condition": "new",
+                      "availability": "Fulfillment by Amazon",
+                      "seller": "For Pete's Sake"
+                    }
+                  ],
+                  "url": "http://www.amazon.com/dp/B0001ZZEKA",
+                  "sku": "B0001ZZEKA",
+                  "name": "amazon.com"
+                },
+                {
+                  "latestoffers": [
+                    {
+                      "price": "26.50",
+                      "lastrecorded_at": 1486434300,
+                      "isactive": 0,
+                      "currency": "USD",
+                      "firstrecorded_at": 1484814700,
+                      "id": "38a0gUNn2O4UGKEWSCQqSu",
+                      "condition": "New",
+                      "availability": "Available",
+                      "seller": "Positive Health"
+                    },
+                    {
+                      "price": "38.20",
+                      "lastrecorded_at": 1486434300,
+                      "shipping": "1541",
+                      "isactive": 0,
+                      "currency": "USD",
+                      "firstrecorded_at": 1480494800,
+                      "id": "79aAsbjcq8ysuUecQCsq60",
+                      "condition": "New",
+                      "availability": "Available",
+                      "seller": "UnbeatableSale"
+                    },
+                    {
+                      "price": "25.56",
+                      "lastrecorded_at": 1480494800,
+                      "isactive": 0,
+                      "currency": "USD",
+                      "firstrecorded_at": 1480494800,
+                      "id": "3jqUy5iGZMwcCwESmYaUyE",
+                      "condition": "New",
+                      "availability": "Available",
+                      "seller": "Tee Gee-s Corner"
+                    }
+                  ],
+                  "url": "http://www.rakuten.com/prod/229825045.html",
+                  "name": "rakuten.com",
+                  "sku": "229825045",
+                  "recentoffers_count": 0
+                }
+              ],
+              "description": "Zone Perfect® Chocolate Peanut Butter Nutrition Ba... (visit site URLs for full description)",
+              "features": {
+                "blob": "Important - Refrigerated items are perishable and tend to have short shelf lives - some of these can be as little as two weeks from the date of receipt - if you are ordering a large quantity, or are otherwise concerned about expiration dates, consider a frozen product."
+              },
+              "images_total": 1,
+              "category": "Breakfast & Cereal Bars"
+            }
+          ],
+          "total_results_count": 1
+        },
+    ]
+}
---
+++ pi_pantry/views/pantry.py
@@ -0,0 +1,11 @@
+from pyramid.security import NO_PERMISSION_REQUIRED
+from pyramid.view import view_config, API_URL
+
+
+@view_config(
+    route_name='manage_items',
+    renderer='../templates/manage_items.jinja2',
+    request_method=('GET', 'POST'),
+    permission=NO_PERMISSION_REQUIRED,)
+def index_view(request):
+    return {}
