46bd9a372bc33c40e42871f43e56c5e34cd985ef
George Ceja <net.george.sky@gmail.com>
2018-04-16 14:53:52 -0700

    setup up scaffold for post and delete requests

--- pi_pantry/views/pantry.py
+++ pi_pantry/views/pantry.py
@@ -1,12 +1,20 @@
 from pyramid.security import NO_PERMISSION_REQUIRED
 from pyramid.view import view_config
+from default import sem3
 
 
 @view_config(
-    route_name='manage_items',
-    renderer='../templates/manage_items.jinja2',
-    request_method=('GET', 'POST'),
+    route_name='manage_item',
+    renderer='../templates/manage_item.jinja2',
+    request_method=('POST', 'DELETE'),
     permission=NO_PERMISSION_REQUIRED,)
-def index_view(request):
-
-    return {}
+def add_item(request):
+    # UPLOAD ITEM TO USERS ACCOUNT
+    if request.method == 'POST':
+        try:
+            item_upc = request.GET['upc']
+        except KeyError:
+            return {}
+    # Delete item from users pantry
+    if request.method == 'DELETE':
+        pass
