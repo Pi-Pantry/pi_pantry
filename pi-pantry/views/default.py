2fc91abb0408f3f95890bae125233a41154f6339
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-17 08:59:45 -0700

    error pi_pantry has no attribute main

--- pi-pantry/views/default.py
+++ pi-pantry/views/default.py
@@ -1,33 +1,50 @@
+from sqlalchemy.exc import DBAPIError
 from pyramid.response import Response
 from pyramid.view import view_config
-
-from sqlalchemy.exc import DBAPIError
+from pyramid.response import Response
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound, HTTPBadRequest
+from ..sample_data import MOCK_DATA
+import requests
+import json
 
 from ..models import MyModel
 
-
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'pi_pantry'}
+API_URL = 'https://api.iextrading.com/1.0'
 
 
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+@view_config(
+    route_name='home',
+    renderer='../templates/base.jinja2',
+    request_method='GET')
+def index_view(request):
+    """
+    Directs user to the home template
+    """
+    return {}
 
-1.  You may need to run the "initialize_pi_pantry_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
+@view_config(
+    route_name='portfolio',
+    renderer='../templates/portfolio.jinja2',
+    request_method='GET')
+def portfolio_view(request):
+    """
+    Directs user to their portfolio template
+    """
+    return {'data': MOCK_DATA}
 
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+
+@view_config(
+    route_name='detail',
+    renderer='../templates/detail.jinja2',
+    request_method='GET')
+def detail_view(request):
+    """
+    Directs user to the detail template
+    """
+    symbol = request.matchdict['symbol']
+
+    for data in API_URL:
+        if data['symbol'] == symbol:
+            return {'data': data}
+    return {}
