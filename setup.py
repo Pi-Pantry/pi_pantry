b3fcc754c3976129a11350d3ee2b02b3cda951bf
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-17 12:56:20 -0700

    scott was here

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open(os.path.join(here, 'README.txt')) as f:
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
+reqs = install_reqs
 
 requires = [
     'plaster_pastedeploy',
