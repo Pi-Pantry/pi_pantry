e2c3c16d249c482c5c1454be64ebde2f384a36f1
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-16 19:04:54 -0700

    added the psycopg2 pkg, updated requirements.txt

--- pi_pantry/models/product.py
+++ pi_pantry/models/product.py
@@ -8,10 +8,10 @@ from .assoc_table import association_table
 from .meta import Base
 
 
-class Product:
+class Product(Base):
     __tablename__ = 'product'
     id = Column(Integer, primary_key=True)
-    account = relationship('Account', secondary=association_table)
+    # account = relationship('Account', secondary=association_table)
     upc = Column(String, nullable=False, unique=True)
     name = Column(String, nullable=False)
     brand = Column(String)
