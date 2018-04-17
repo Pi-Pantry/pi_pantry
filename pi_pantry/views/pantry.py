aa7eb1900c32afbe6c9b567e79cb4bcd35469a2f
George Ceja <net.george.sky@gmail.com>
2018-04-16 17:34:27 -0700

    Added to view_confid

--- pi_pantry/views/pantry.py
+++ pi_pantry/views/pantry.py
@@ -1,20 +1,28 @@
-from pyramid.security import NO_PERMISSION_REQUIRED
+# from pyramid.security import NO_PERMISSION_REQUIRED
+from pyramid.reponse import Response
+from sqlalchemy.exc import DBAPIError
+from ..models import Product, Account
 from pyramid.view import view_config
+from . import DB_ERR_MSG
 from default import sem3
 
 
 @view_config(
-    route_name='manage_item',
-    renderer='../templates/manage_item.jinja2',
-    request_method=('POST', 'DELETE'),
-    permission=NO_PERMISSION_REQUIRED,)
-def add_item(request):
-    # UPLOAD ITEM TO USERS ACCOUNT
-    if request.method == 'POST':
+    route_name='add',
+    renderer='../templates/manage_items.jinja2')
+def manage_items_view(request):
+    if request.method == 'GET':
         try:
-            item_upc = request.GET['upc']
+            upc = request.GET['upc']
         except KeyError:
             return {}
-    # Delete item from users pantry
-    if request.method == 'DELETE':
-        pass
+        try:
+            query = request.dbsession.query(Product)
+            upc_data = query.filter(Product.upc == upc).one_or_none()
+        except DBAPIError:
+            return Response(DB_ERR_MSG, content_type='text/plain', status=500)
+
+        if upc_data is None:
+            pass
+
+        return {'product': upc_data}
