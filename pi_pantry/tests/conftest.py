5b6bb555968aaf244a5e3645ede80e2ccd1e7a19
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2018-04-18 18:15:06 -0700

    added test folder and files

---
+++ pi_pantry/tests/conftest.py
@@ -0,0 +1,81 @@
+# import os
+import pytest
+import unittest
+from pyramid import testing
+from ..models.meta import Base
+from ..models import Product
+from ..models import Account
+from ..models import Assoc
+
+
+@pytest.fixture
+def test_entry():
+    """
+    Test stock entry
+    """
+    return Product(
+        upc='011345876435',
+        name='Huge Chocolate Bar',
+        brand='HUGE',
+        description='Giant chocolate bar with chocolate',
+        category='candy',
+        image='https://www.hugecandy.com',
+        size='10 lbs',
+        manufacturer='Huge candy co.',
+    )
+
+
+def test_account():
+    """
+    Test account entry
+    """
+    return Account(
+        username='brandon',
+        password='1234',
+        email='brandon@brandon.brandon'
+    )
+
+
+@pytest.fixture
+def configuration(request):
+    """
+    Setup a database for testing purposes
+    """
+    config = testing.setUp(settings={
+        'sqlalchemy.url': 'postgres://localhost:5432/pantry_test'
+        # 'sqlalchemy.url': os.environ['TEST_DATABASE_URL']
+    })
+    config.include('pi_pantry.models')
+    config.include('pi_pantry.routes')
+
+    def teardown():
+        testing.tearDown()
+
+    request.addfinalizer(teardown)
+    return config
+
+
+@pytest.fixture
+def db_session(configuration, request):
+    """
+    Create a database session for interacting with the test database
+    """
+    SessionFactory = configuration.registry['dbsession_factory']
+    session = SessionFactory()
+    engine = session.bind
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        session.transaction.rollback()
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return session
+
+
+@pytest.fixture
+def dummy_request(db_session):
+    """
+    Create a dummy GET request with a dbsession
+    """
+    return testing.DummyRequest(dbsession=db_session)
