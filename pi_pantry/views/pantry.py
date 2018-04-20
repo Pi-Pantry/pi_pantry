b9a82c8636c26c9b5d566eb100ae3b3eee8fd70c
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-19 20:14:21 -0700

    final css before merge

--- pi_pantry/views/pantry.py
+++ pi_pantry/views/pantry.py
@@ -111,6 +111,7 @@ def manage_items_view(request):
                 product = parse_upc_data(query_data)
                 upc_data = Product(**product)
             except (KeyError, IndexError, Semantics3Error):
+                # import pdb; pdb.set_trace()
                 return {'err': 'UPC not in database.  Enter your own fields below.'}
             try:
                 request.dbsession.add(upc_data)
